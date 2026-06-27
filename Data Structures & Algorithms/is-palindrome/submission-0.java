41fd044910b5991c3719c7020475a988e7fe8412
Vansh Gupta <vanshgupta7096@gmail.com>
2026-06-28 00:04:42 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+         s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+        
+    }
+}
